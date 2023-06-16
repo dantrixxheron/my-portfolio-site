e48acac48394d7cb8e68d7424123fb5f40b7460f
dantrixxheron <123124430+dantrixxheron@users.noreply.github.com>
2023-06-16 05:31:49 +0000

    new section-education

---
+++ src/Components/Education.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+const Education = () => {
+  return (
+    <section id="education">
+      <h2 style={{ textAlign: "center" }}>Education</h2>
+      <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
+        <div style={{ maxWidth: "40%", alignSelf: "center" }}>
+          <img
+            src="https://i.ibb.co/0jZ3Q0K/IMG-20210901-120751.jpg"
+            style={{
+              width: "100%",
+              height: "90%",
+              borderRadius: "10px",
+              boxShadow: "0 0 10px 0 rgba(0, 0, 0, 0.2)",
+            }}
+            alt="Daniela Cruz"
+          />
+        </div>
+        <div style={{ maxWidth: "60%", paddingLeft: "2rem" }}>
+          <h3 style={{ color: "#4E567E" }}>Universidad de Colima</h3>
+          <p className="small"> Bachelor´s degree in Software Engineering</p>
+          <p className="small">2022 - Now</p>
+          <p className="small">Colima, Mexico.</p>
+        </div>
+        <div style={{ maxWidth: "40%", alignSelf: "center" }}>
+          <img
+            src="https://i.ibb.co/0jZ3Q0K/IMG-20210901-120751.jpg"
+            style={{
+              width: "100%",
+              height: "90%",
+              borderRadius: "10px",
+              boxShadow: "0 0 10px 0 rgba(0, 0, 0, 0.2)",
+            }}
+            alt="Daniela Cruz"
+          />
+        </div>
+        <div style={{ maxWidth: "60%", paddingLeft: "2rem" }}>
+          <h3 style={{ color: "#4E567E" }}>Universidad de Colima</h3>
+          <p className="small"> Bachelor´s degree in Software Engineering</p>
+          <p className="small">2022 - Now</p>
+          <p className="small">Colima, Mexico.</p>
+        </div>
+        <div style={{ maxWidth: "40%", alignSelf: "center" }}>
+          <img
+            src="https://i.ibb.co/0jZ3Q0K/IMG-20210901-120751.jpg"
+            style={{
+              width: "100%",
+              height: "90%",
+              borderRadius: "10px",
+              boxShadow: "0 0 10px 0 rgba(0, 0, 0, 0.2)",
+            }}
+            alt="Daniela Cruz"
+          />
+        </div>
+        <div style={{ maxWidth: "60%", paddingLeft: "2rem" }}>
+          <h3 style={{ color: "#4E567E" }}>Universidad de Colima</h3>
+          <p className="small"> Bachelor´s degree in Software Engineering</p>
+          <p className="small">2022 - Now</p>
+          <p className="small">Colima, Mexico.</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Education;
