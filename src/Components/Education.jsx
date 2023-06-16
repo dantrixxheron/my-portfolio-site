28576d89b8b9f5cfdd43116759aebd37b536f161
dantrixxheron <123124430+dantrixxheron@users.noreply.github.com>
2023-06-16 19:49:11 +0000

    education-info

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -1,61 +1,47 @@
 import React from "react";
+
+import imageUniversity from "../images/universityLogo.png";
+
+const imageUniAltText = "University of Colima's logo";
 const Education = () => {
   return (
     <section id="education">
       <h2 style={{ textAlign: "center" }}>Education</h2>
-      <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
+      <div style={{ display: "flex", flexDirection: "row" }}>
         <div style={{ maxWidth: "40%", alignSelf: "center" }}>
           <img
-            src="https://i.ibb.co/0jZ3Q0K/IMG-20210901-120751.jpg"
+            src={imageUniversity}
             style={{
               width: "100%",
-              height: "90%",
+              height: "40%",
               borderRadius: "10px",
               boxShadow: "0 0 10px 0 rgba(0, 0, 0, 0.2)",
             }}
-            alt="Daniela Cruz"
+            alt={imageUniAltText}
           />
         </div>
-        <div style={{ maxWidth: "60%", paddingLeft: "2rem" }}>
+        <div style={{ maxWidth: "60%", padding: "2rem" }}>
           <h3 style={{ color: "#4E567E" }}>Universidad de Colima</h3>
-          <p className="small"> Bachelor´s degree in Software Engineering</p>
+          <p className="small"> Bachelor&apos;s degree in Software Engineering</p>
           <p className="small">2022 - Now</p>
           <p className="small">Colima, Mexico.</p>
         </div>
         <div style={{ maxWidth: "40%", alignSelf: "center" }}>
           <img
-            src="https://i.ibb.co/0jZ3Q0K/IMG-20210901-120751.jpg"
+            src={imageUniversity}
             style={{
               width: "100%",
               height: "90%",
               borderRadius: "10px",
               boxShadow: "0 0 10px 0 rgba(0, 0, 0, 0.2)",
             }}
-            alt="Daniela Cruz"
+            alt={imageUniAltText}
           />
         </div>
-        <div style={{ maxWidth: "60%", paddingLeft: "2rem" }}>
+        <div style={{ maxWidth: "60%", padding: "2rem" }}>
           <h3 style={{ color: "#4E567E" }}>Universidad de Colima</h3>
-          <p className="small"> Bachelor´s degree in Software Engineering</p>
-          <p className="small">2022 - Now</p>
-          <p className="small">Colima, Mexico.</p>
-        </div>
-        <div style={{ maxWidth: "40%", alignSelf: "center" }}>
-          <img
-            src="https://i.ibb.co/0jZ3Q0K/IMG-20210901-120751.jpg"
-            style={{
-              width: "100%",
-              height: "90%",
-              borderRadius: "10px",
-              boxShadow: "0 0 10px 0 rgba(0, 0, 0, 0.2)",
-            }}
-            alt="Daniela Cruz"
-          />
-        </div>
-        <div style={{ maxWidth: "60%", paddingLeft: "2rem" }}>
-          <h3 style={{ color: "#4E567E" }}>Universidad de Colima</h3>
-          <p className="small"> Bachelor´s degree in Software Engineering</p>
-          <p className="small">2022 - Now</p>
+          <p className="small"> Associate Degree in Analyst Programmer </p>
+          <p className="small">2019 - 2022 </p>
           <p className="small">Colima, Mexico.</p>
         </div>
       </div>
